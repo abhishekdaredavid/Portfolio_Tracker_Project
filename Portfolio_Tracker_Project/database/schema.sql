1dc6a9d3e392f3284a43a2aeebc131302a47b884
abhishekdaredavid <146186138+abhishekdaredavid@users.noreply.github.com>
2024-12-19 20:17:18 +0530

    Update schema.sql

--- Portfolio_Tracker_Project/database/schema.sql
+++ Portfolio_Tracker_Project/database/schema.sql
@@ -1,7 +1,10 @@
-CREATE TABLE stocks (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE DATABASE portfolio;
+USE portfolio;
+
+CREATE TABLE stock (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255),
-  ticker VARCHAR(10),
+  ticker VARCHAR(50),
   quantity INT,
   buy_price DECIMAL(10, 2)
 );
